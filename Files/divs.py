7fb366e990aa2c77888c69f017b2f9bb9f0513d6
Ayodo <37110623+ayoodo@users.noreply.github.com>
2022-03-29 21:10:58 +0100

    start files

---
+++ Files/divs.py
@@ -0,0 +1,33 @@
+import pickle as pickle
+def saisie():
+    global n
+    v = False
+    while not v:
+        n = int(input("Donner un entier: "))
+        v = n > 1
+
+def divs(nomphy, n):
+    F = open(nomphy, "wb")
+    for i in range(1,(n//2)+1, 2): # Pour le nombre premier (1, (n//2)+1, 2)
+        if n % i == 0:
+            pickle.dump(i, F)
+    pickle.dump(n, F)
+    F.close()
+
+def output(nomphy):
+    F = open(nomphy, "rb")
+    Fin_Fichier = False
+    while not Fin_Fichier:
+        try:
+            print(pickle.load(F))
+        except:
+            Fin_Fichier = True
+    F.close()
+
+
+
+# saisie()
+n = 20
+nomphy = "divs.dat"
+divs(nomphy, n)
+output(nomphy)
